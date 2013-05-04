c5e4f9a30e51a46addc9f22e6cb2460d307cc94c
Floor Drees <floordrees@gmail.com>
2013-05-04 20:01:42 +0200

    add tags spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper :exhibits
   
+  def blog_url(*) 
+    root_url
+  end
+  
   before_filter :init_blog 
   private
     def init_blog 
